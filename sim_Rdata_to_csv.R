5b1e033c1dd971320cff7330374d334eb390c6f9
Savannah Bergquist <bergquis@hcp.med.harvard.edu>
2016-12-12 10:47:05 -0500

    Initial commit

---
+++ sim_Rdata_to_csv.R
@@ -0,0 +1,5 @@
+load("W:/DATA/markscan_authorized_users/bergquist/EI/df_sim_250.RData")
+load("W:/DATA/markscan_authorized_users/bergquist/EI/df_sim_full.RData")
+
+save(df_sim_250, file="C:/Users/bergquis/Dropbox/Evil Insurers Draft/unprofits/df_sim_250.csv")
+save(df_sim, file="C:/Users/bergquis/Dropbox/Evil Insurers Draft/unprofits/df_sim.csv")
---
+++ unprofitability_SL.R
@@ -0,0 +1,126 @@
+### Unprofitability: Super Learner
+library(SuperLearner)
+library(parallel)
+detectCores()
+library(doMC)
+options(mc.cores=5)
+
+
+## Read in full sample of drug data from TADS (2013)
+data <- read.csv("/data/markscan_authorized_users/bergquist/EI/drugs_person_values3.csv", header=T)
+
+# check the data
+summary(data)
+
+# min and max for all variables besides unprofits
+max(data[,-1])
+min(data[,-1])
+
+# max and min for unprofits 
+max(data[,1])
+min(data[,1])
+
+
+set.seed(27)
+# get rid of variables with no obs
+newdat <-data[, colSums(data != 0) > 0] 
+
+dim(newdat)
+cSums<-colSums(newdat)
+
+# "screener" for taking out therapeutic class variables
+tgrp.fun <- function(X, ...){
+  whichvars <- c(rep.int(TRUE, ncol(X)))
+  names(whichvars) <- colnames(X)
+  tclsvars <- grep("tcls", names(X), value=T)
+  whichvars[tclsvars] <- FALSE 
+  whichvars <- unname(whichvars)
+  return(whichvars) 
+}
+
+# lasso screener that always retains classes for HIV and MS drugs
+var.index <- c(which(colnames(newdat)=="tcls14"), which(colnames(newdat)=="tcls251"))
+
+screen.glmnet10 <- function(Y, X, family, alpha = 1, minscreen = 2, nfolds = 10, nlambda = 100,fixed.var.index=var.index,...) {
+  # .SL.require('glmnet')
+  if(!is.matrix(X)) {
+    X <- model.matrix(~ -1 + ., X)
+  }
+  fitCV <- glmnet::cv.glmnet(x = X, y = Y, lambda = NULL, type.measure = 'deviance', 
+                             nfolds = nfolds, family = family$family, alpha = alpha, 
+                             nlambda = nlambda, pmax=10, parallel=T)
+  whichVariable <- (as.numeric(coef(fitCV$glmnet.fit, s = fitCV$lambda.min))[-1] != 0)
+  # the [-1] removes the intercept; taking the coefs from the fit w/ lambda that gives minimum cvm
+  if (sum(whichVariable) < minscreen) {
+    warning("fewer than minscreen variables passed the glmnet screen, 
+            increased lambda to allow minscreen variables")
+    sumCoef <- apply(as.matrix(fitCV$glmnet.fit$beta), 2, function(x) sum((x != 0)))
+    newCut <- which.max(sumCoef >= minscreen) 
+    whichVariable <- (as.matrix(fitCV$glmnet.fit$beta)[, newCut] != 0)
+  }
+  whichVariable[c(var.index)] <- TRUE
+  return(whichVariable)
+}
+
+# lasso
+SL.glmnet1 <- function(Y, X, newX, family, obsWeights, id, alpha = 1, nfolds = 10, nlambda = 100, useMin = TRUE, ...) {
+  #.SL.require('glmnet')
+  # X must be a matrix, should we use model.matrix or as.matrix
+  if(!is.matrix(X)) {
+    X <- model.matrix(~ -1 + ., X)
+    newX <- model.matrix(~ -1 + ., newX)
+  }
+  # now use CV to find lambda
+  fitCV <- glmnet::cv.glmnet(x = X, y = Y, weights = obsWeights, lambda = NULL, type.measure = 'deviance', nfolds = nfolds, family = family$family, alpha = alpha, nlambda = nlambda, parallel=T)
+  # two options for lambda, fitCV$lambda.min and fitCV$lambda.1se
+  pred <- predict(fitCV$glmnet.fit, newx = newX, s = ifelse(useMin, fitCV$lambda.min, fitCV$lambda.1se), type = 'response')
+  fit <- list(object = fitCV, useMin = useMin)
+  class(fit) <- 'SL.glmnet'
+  out <- list(pred = pred, fit = fit)
+  return(out)
+}
+
+# ridge
+SL.glmnet0 <- function(Y, X, newX, family, obsWeights, id, alpha = 0, nfolds = 10, nlambda = 100, useMin = TRUE, ...) {
+  #.SL.require('glmnet')
+  # X must be a matrix, should we use model.matrix or as.matrix
+  if(!is.matrix(X)) {
+    X <- model.matrix(~ -1 + ., X)
+    newX <- model.matrix(~ -1 + ., newX)
+  }
+  # now use CV to find lambda
+  fitCV <- glmnet::cv.glmnet(x = X, y = Y, weights = obsWeights, lambda = NULL, type.measure = 'deviance', nfolds = nfolds, family = family$family, alpha = alpha, nlambda = nlambda, parallel=T)
+  # two options for lambda, fitCV$lambda.min and fitCV$lambda.1se
+  pred <- predict(fitCV$glmnet.fit, newx = newX, s = ifelse(useMin, fitCV$lambda.min, fitCV$lambda.1se), type = 'response')
+  fit <- list(object = fitCV, useMin = useMin)
+  class(fit) <- 'SL.glmnet'
+  out <- list(pred = pred, fit = fit)
+  return(out)
+}
+
+alg_screen <- list(c("SL.nnet", "All", "tgrp.fun", "screen.glmnet10"), 
+                   c("SL.glmnet1", "All", "tgrp.fun", "screen.glmnet10"),
+                   c("SL.glm", "All", "tgrp.fun", "screen.glmnet10"),
+                   c("SL.glmnet0", "All", "tgrp.fun", "screen.glmnet10"),
+                   c("SL.rpart", "All", "tgrp.fun", "screen.glmnet10"))
+
+set.seed(99)
+start <- proc.time()
+fit.data.SL <- SuperLearner(Y=newdat[,1], X=newdat[,-1], SL.library=alg_screen, family=gaussian(),method="method.NNLS", verbose=TRUE)
+total <- proc.time()-start
+print(total)
+print(fit.data.SL)
+
+SLPreds<-fit.data.SL$SL.predict
+coef<-fit.data.SL$coef
+cvRisk<-fit.data.SL$cvRisk
+Z<-fit.data.SL$Z
+fitLibrary<-fit.data.SL$fitLibrary
+
+save(fit.data.SL, file="/data/markscan_authorized_users/bergquist/EI/fullfitv9.Rdata")
+save(cSums, file="/data/markscan_authorized_users/bergquist/EI/fullcSumsv9.Rdata")
+save(SLPreds, file="/data/markscan_authorized_users/bergquist/EI/fullSLPredsv9.Rdata")
+save(coef, file="/data/markscan_authorized_users/bergquist/EI/fullcoefv9.Rdata")
+save(cvRisk, file="/data/markscan_authorized_users/bergquist/EI/fullcvRiskv9.Rdata")
+save(Z, file="/data/markscan_authorized_users/bergquist/EI/fullZv9.Rdata")
+save(fitLibrary, file="/data/markscan_authorized_users/bergquist/EI/fullfitLibraryv9.Rdata")
