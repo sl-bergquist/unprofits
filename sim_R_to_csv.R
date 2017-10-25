e54f7314137f784f30d92b7a26418abb8b9308c6
Savannah Bergquist <sl-bergquist@users.noreply.github.com>
2017-10-25 09:21:08 -0400

    Delete sim_R_to_csv.R

--- sim_R_to_csv.R
+++
@@ -1,7 +0,0 @@
-
-load("W:/DATA/markscan_authorized_users/bergquist/EI/df_sim_full.RData")
-load("W:/DATA/markscan_authorized_users/bergquist/EI/df_sim_250.RData")
-
-
-write.csv(df_sim_250, file="C:/Users/bergquis/Dropbox/Evil_Insurers_Draft/unprofits/df_sim_250.csv")
-write.csv(df_sim, file="C:/Users/bergquis/Dropbox/Evil_Insurers_Draft/unprofits/df_sim.csv")
